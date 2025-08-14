c7284b2928c09ed2f5035529f4e68e110043526d
Debiprasad Brahma <brahmadebiprasad@gmail.com>
2025-08-14 23:32:36 +0530

    Update ans.java

--- Array/15. 3Sum/ans.java
+++ Array/15. 3Sum/ans.java
@@ -1,5 +1,4 @@
 // Leet Code
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
        List<List<Integer>> ans = new ArrayList<>();
@@ -25,5 +24,44 @@ class Solution {
        }
 
        return ans;
+    }
+}
+
+
+
+// GFG
+class Solution {
+    public static ArrayList<ArrayList<Integer>> triplets(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<arr.length-2; i++){
+            if (i > 0 && arr[i] == arr[i - 1]) continue; // Skip duplicate first element
+            
+            int val = arr[i];
+            int left = i+1;
+            int right = arr.length - 1;
+            
+            while(left < right){
+                int total = val + arr[left] + arr[right];
+                if(total == 0){
+                    
+                    ArrayList<Integer> triplet = new ArrayList<>();
+                    triplet.add(val);
+                    triplet.add(arr[left]);
+                    triplet.add(arr[right]);
+                    ans.add(triplet);
+                    
+                    while(left<right && arr[left] == arr[left+1]) left++;
+                    while(left<right && arr[right] == arr[right-1]) right--;
+                    
+                    left++;
+                    right--;
+                }
+                else if(total < 0) left++;
+                else right--;
+            }
+        }
+        return ans;
     }
 }
